1eb25c214e742aeafa81f0836a4d7249456409f9
Antony Woods <antony@mastodonc.com>
2016-06-29 15:48:31 +0100

    Add tests and update gitignore

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.385"]
-                 [org.onyxplatform/onyx "0.9.6"]
+                 [org.onyxplatform/onyx "0.9.4"]
                  [amazonica "0.3.61"]
                  [com.taoensso/carmine "2.12.2"]]
   :main ^:skip-aot witan.workspace-onyx-plugins
